213955a8e7a1414ac8136dcfe4165f63e7a560e5
MateoBrena <matubrena@gmail.com>
2022-07-25 15:56:39 -0300

    Index y Register creados

---
+++ Anatomy/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 3030;
+const start = () => console.log('Starting server...');
+server.listen(port,start());
+
+const path = require('path');
+
+const public = path.join(__dirname, 'public');
+
+const statics = express.static(public);
+
+server.use(statics);
+
+
+server.get('/', function (req, res) {
+    let file = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(file);
+})
+
+server.get('/productDetail', function (req, res) {
+    let file = path.join(__dirname, 'views', 'productDetail.html');
+    res.sendFile(file);
+})
+
+server.get('/productCart', function (req, res) {
+    let file = path.join(__dirname, 'views', 'productCart.html');
+    res.sendFile(file);
+})
+
+server.get('/login', function (req, res) {
+    let file = path.join(__dirname, 'views', 'login.html');
+    res.sendFile(file);
+})
+
+server.get('/register', function (req, res) {
+    let file = path.join(__dirname, 'views', 'register.html');
+    res.sendFile(file);
+})
