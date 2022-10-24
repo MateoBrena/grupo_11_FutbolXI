8c1a98be7bf99bb3f2c4cf78ab5fee7628ae1c41
Jeremías Rombolá <jere.rombola@gmail.com>
2022-10-23 21:05:18 -0300

    modificaciones visuales en validacion del front-end

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -1,3 +1,4 @@
+
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let login = document.querySelector("#login");
@@ -7,7 +8,7 @@ let validRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]
 let form = document.querySelector(".formulario-registro")
 
 login.addEventListener("click", function(event){
-
+    
     event.preventDefault();
     let errores = {};
     if(email.value.length < 1 ){
@@ -16,15 +17,21 @@ login.addEventListener("click", function(event){
     if (email.value.length >= 1 && !email.value.match(validRegex) ){
     errores.email = "Email no valido"
     }
+       
+
     if(password.value.length < 1 ){
    errores.password = "Este campo debe estar completo";
     }
-
+    
     if(password.value.length < 4 && password.value.length > 0 ){
         errores.password = "El minimo debe ser 4 caracteres";
     }
+
     if(Object.keys(errores).length >= 1){
+        
         errorEmail.innerText = errores.email ? errores.email : null;
+        errorEmail.style.backgroundColor+= "tomato"
         errorPassword.innerText = errores.password ? errores.password : null;
+        errorPassword.style.backgroundColor+= "tomato"
     } else { form.submit() }
 })
