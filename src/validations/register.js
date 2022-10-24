6253a90eba48ea99d1fd5aee1476f3b94a26fe1f
MauricioDuch <mauricioduch06@gmail.com>
2022-10-23 21:42:00 -0300

    Validaciones: Email y de min 8 caracteres password

--- src/validations/register.js
+++ src/validations/register.js
@@ -1,13 +1,27 @@
 const {body} = require("express-validator");
+const {User} = require("../database/models/index");
 
-let email = body("email").notEmpty().withMessage("Email no puede quedar vacío").bail().isEmail().withMessage("Email no válido")
-let clave = body("clave").notEmpty().withMessage("Debes ingresar una contraseña").bail().isLength({min:4}).withMessage("Mínimo 4 caraceteres")
-let confirmarClave = body("confirmarClave").notEmpty().withMessage("Debes confirmar la contraseña").bail().isLength({min:4}).withMessage("Mínimo 4 caraceteres").custom((value,{req}) => {
+let email = body("email").notEmpty().withMessage("Email no puede quedar vacío").bail().isEmail().withMessage("Email no válido").custom((value,{req}) => {
+  let user = User.findOne({where:{email: req.body.email}}).then(userFound=>
+      {if(!userFound){
+        return true
+      }
+      throw new Error("Email ya registrado")
+      
+  })
+  return user
+})
+
+let clave = body("clave").notEmpty().withMessage("Debes ingresar una contraseña").bail().isLength({min:8}).withMessage("Mínimo 8 caraceteres")
+
+let confirmarClave = body("confirmarClave").notEmpty().withMessage("Debes confirmar la contraseña").bail().isLength({min:8}).withMessage("Mínimo 4 caraceteres").custom((value,{req}) => {
     if (value !== req.body.clave) {
       throw new Error('La confirmación no coincide con la contraseña');
     }
     return true;
   })
+
+
 
 let validaciones = [email,clave,confirmarClave]
 
